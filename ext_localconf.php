b6c6265fa4ed072d3153481b4324fbc341fa130c
Frank Nägler <frank.naegler@typo3.com>
2020-11-16 15:18:54 +0100

    [TASK] Prepare extension for usages with TYPO3 v11 (#2)
    * [TASK] Use PSR-14 events instead of signals
    
    * [TASK] Allow usage with TYPO3 11.0.x-dev
    
    * [TASK] Test also with current master branch
    
    * [TASK] Fix github actions
    
    * [BUGFIX] Set minimum stability to dev
    
    * [BUGFIX] Use Typo3Version class as fallbback
    
    * [TASK] Change version contraint
    
    Co-authored-by: Benjamin Kott <benjamin.kott@outlook.com>

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,35 +5,17 @@ if (!defined('TYPO3_MODE')) {
 }
 
 call_user_func(function () {
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-    $signalSlotDispatcher
-        ->connect(
-            \TYPO3\CMS\Core\Resource\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileAdd,
-            \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileAdd
-        );
-    $signalSlotDispatcher
-        ->connect(
-            \TYPO3\CMS\Core\Resource\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileReplace,
-            \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileReplace
-        );
-    $signalSlotDispatcher
-        ->connect(
-            \TYPO3\CMS\Core\Resource\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PostFileSetContents,
-            \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
-            \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PostFileSetContents
-        );
+    $typo3Version = (defined('TYPO3_version'))
+        ? TYPO3_version
+        : \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class)->getVersion();
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Core\Utility\GeneralUtility']['moveUploadedFile'][]
         = \T3G\SvgSanitizer\Hooks\GeneralUtilityHook::class . '->processMoveUploadedFile';
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['T3G\SvgSanitizer\Updates\SanitizeExistingSVG']
         = \T3G\SvgSanitizer\Updates\v8\SanitizeExistingSVG::class;
-    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9005000) {
+
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($typo3Version) >= 9005000) {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['T3G\SvgSanitizer\Updates\SanitizeExistingSVG']
             = \T3G\SvgSanitizer\Updates\v9\SanitizeExistingSVG::class;
     }
@@ -42,4 +24,30 @@ call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processUpload']['svg_sanitizer']
         = \T3G\SvgSanitizer\Hooks\DataHandlerHook::class;
 
+    // The following hooks/signal have been deprecated in 10.2 and removed with v11:
+    // As a replacement for the deprecated signals we introduced the according PSR-14 events.
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($typo3Version) < 1002000) {
+        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+        $signalSlotDispatcher
+            ->connect(
+                \TYPO3\CMS\Core\Resource\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileAdd,
+                \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileAdd
+            );
+        $signalSlotDispatcher
+            ->connect(
+                \TYPO3\CMS\Core\Resource\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileReplace,
+                \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileReplace
+            );
+        $signalSlotDispatcher
+            ->connect(
+                \TYPO3\CMS\Core\Resource\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PostFileSetContents,
+                \T3G\SvgSanitizer\SignalSlot\ResourceStorage::class,
+                \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PostFileSetContents
+            );
+    }
 });
